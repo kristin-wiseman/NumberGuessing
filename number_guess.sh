676e89daaa1c221e46951f82449dd80bc85d4af8
Kristin Wiseman <kristin.w.dev@gmail.com>
2024-12-02 02:16:40 +0000

    feat: guess should be an int.

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PSQL="psql -X --username=freecodecamp --dbname=number_guess -t -c"
 
+# $NUMBER isn't completely POSIX compliant
 NUMBER=$(( (RANDOM % 1000) + 1 ))
 
 echo "Enter your username:"
@@ -8,6 +9,7 @@ read USERNAME
 
 USER=$($PSQL "SELECT games_played, best_game FROM users WHERE username = '$USERNAME';")
 
+# if no user found
 if [[ -z $USER ]]; then
   echo "Welcome $USERNAME! It looks like this is your first time here."
   ADD_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME');")
@@ -16,4 +18,19 @@ else
   do
     echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   done
+fi
+
+NUM_GUESSES=0
+echo "Guess the secret number between 1 and 1000:"
+read GUESS
+
+# if not an integer
+while [[ ! $GUESS =~ ^[0-9]+$ ]]; do
+  echo "That is not an integer, guess again:"
+  read GUESS
+done
+
+if [[ "$GUESS" -eq "$NUMBER" ]]; then
+  NUM_GUESSES=$(( NUM_GUESSES + 1 ))
+  echo "You guessed it in $NUM_GUESSES tries. The secret number was $NUMBER. Nice job! "
 fi
